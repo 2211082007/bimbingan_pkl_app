b7ed77a84452f49f5e497cc159c0ccf7a7a6340f
2211082007 <fadilaislmns22@gmail.com>
2025-07-17 11:41:53 +0700

    logo baru

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,15 +1,16 @@
 <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
-  <div class="app-brand demo">
+ <div class="app-brand demo">
     <a href="{{ url('/') }}" class="app-brand-link">
-      <span class="app-brand-logo demo">
-        <!-- Logo SVG -->
-      </span>
-      <span class="app-brand-text demo menu-text fw-bolder ms-2">Bimbingan PKL</span>
+        <span class="app-brand-logo demo">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" width="200" height="60">
+        </span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
-      <i class="bx bx-chevron-left bx-sm align-middle"></i>
+        <i class="bx bx-chevron-left bx-sm align-middle"></i>
     </a>
-  </div>
+</div>
+
+
 
   <div class="menu-inner-shadow"></div>
 
